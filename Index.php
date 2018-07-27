b6b49f244f6c775df3b7c72efa92d48de51eb2ab
Benancio <benancio@alliancesthatwork.com>
2018-07-27 13:36:45 -0500

    index

--- Index.php
+++ Index.php
@@ -11,7 +11,7 @@
 <nav class="navbar navbar-default">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">WebSiteName</a>
+      <a class="navbar-brand" href="#">WebSiteNameeregegtyhy</a>
     </div>
     <ul class="nav navbar-nav">
       <li class="active"><a href="hola.php">Home</a></li>
